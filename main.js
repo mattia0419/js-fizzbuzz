450fa800c44128904c69a1b09ec0de353397978a
mattia0419 <pironemattia6@gmail.com>
2023-07-21 18:34:03 +0200

    add css

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 const numRipetitions = 100;
 const list = document.getElementById("mylist");
+
 for(let i = 1; i <= numRipetitions; i++){
-    
+    const div_list = document.createElement("div")
+    list.append(div_list)
     
     if(i % 5 == 0 && i % 3 == 0){
-        list.innerHTML += `FizzBuss `
+        div_list.innerHTML += `FizzBuzz `;
+        div_list.classList.add("box");
+        div_list.classList.add("bg-fizzbuzz");
     }
     else if(i % 5 == 0){
-        list.innerHTML += `Fizz `
+        div_list.innerHTML += `Fizz `;
+        div_list.classList.add("box");
+        div_list.classList.add("bg-fizz");
     }
     else if(i % 3 == 0){
-        list.innerHTML += `Buss `
+        div_list.innerHTML += `Buzz `;
+        div_list.classList.add("box");
+        div_list.classList.add("bg-buzz");
     }
     
-    list.innerHTML += `${i + " "}`
+    else {
+        div_list.innerHTML += `${i + " "}`;
+        div_list.classList.add("box");
+        div_list.classList.add("bg-normal")
+    }
+    
+    console.log(div_list);
+    
 }
